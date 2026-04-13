d3de3cd2e1857884c62925a6820975de3c4e8c23
sindrebakke <sindre.bakke@revolve.no>
2026-04-13 18:36:54 +0200

    fixes in uart c

--- 2_uart/uart.c
+++ 2_uart/uart.c
@@ -1,4 +1,6 @@
 #include <stdint.h>
+#include <stdio.h>
+#include <sys/types.h>
 
 #include "gpio.h"
 #include "uart.h"
@@ -11,50 +13,48 @@
 #define DISCONNECTED 0xFFFFFFFF
 
 void uart_init() {
-
-    GPIO->PIN_CNF[UART_TX_PIN] = (0 << 1);
-    GPIO->PIN_CNF[UART_RX_PIN] = (0 << 1);
+    GPIO->PIN_CNF[UART_TX_PIN] = 1;
+    GPIO->PIN_CNF[UART_RX_PIN] = 0;
 
     UART->PSELTXD = UART_TX_PIN;
     UART->PSELRXD = UART_RX_PIN;
 
-    UART->PSELCTS = DISCONNECTED;
-    UART->PSELRTS = DISCONNECTED;
-
     UART->BAUDRATE = Baud9600;
 
+    UART->PSELRTS = DISCONNECTED;
+    UART->PSELCTS = DISCONNECTED;
+
     UART->ENABLE = 8;
-    UART->TASKS_STARTRX = 1;
+    UART->TASKS_STARTTX = 1;
 }
 
 void uart_send(char letter){
     UART->TASKS_STARTTX = 1;
     UART->TXD = letter;
-
     while (!UART->EVENTS_TXDRDY){
-
     }
     UART->TASKS_STOPTX = 1;
+    UART->TASKS_STARTTX = 0;
     UART->EVENTS_TXDRDY = 0;
 }
 
 char uart_read(){
-
     UART->TASKS_STARTRX = 1;
-
-    if (UART->EVENTS_RXDRDY){
+    if (UART->EVENTS_RXDRDY != 0) {
         UART->EVENTS_RXDRDY = 0;
-        return UART->RXD;
-    }
-    else{
+        char letter = UART->RXD;
+        return letter;
+    } else {
         return '\0';
-    };
+    }
 }
 
 // Redirect printf to UART
-int _write(int file, char *ptr, int len) {
-    for (int i = 0; i < len; i++) {
-        uart_send(ptr[i]);
+ssize_t _write(int fd, const void *buf, size_t count){
+    char * letter = (char *)(buf);
+    for(int i = 0; i < count; i++){
+        uart_send(*letter);
+        letter++;
     }
-    return len;
+    return count;
 }
