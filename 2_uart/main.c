6d40996277d10ae11b6c14cfbbc849072af08015
Anders Sandvik <anders-01@live.no>
2026-04-13 14:15:14 +0200

    Task 2.2.4

--- 2_uart/main.c
+++ 2_uart/main.c
@@ -1,14 +1,51 @@
 #include <stdint.h>
+#include <stdio.h>
 #include "uart.h"
+#include "gpio.h"
+
+#define BUTTON1_PIN 13
+#define BUTTON2_PIN 14
+
+void button_init(){ 
+	GPIO->PIN_CNF[13] = (3 << 2);
+	GPIO->PIN_CNF[14] = (3 << 2);
+}
 
 
 int main() {
 
+    for(int i = 17; i <= 20; i++){
+		GPIO->DIRSET = (1 << i);
+		GPIO->OUTCLR = (1 << i);
+	} // Configure LED matrix
 
-    
+    uart_init();
+    button_init();
+
 	int sleep = 0;
 
-	while(1){
+    char A = 'A';
+    char B = 'B';
 
+	while(1){
+        if (!(GPIO->IN & (1 << 13))){
+			uart_send(A);
+		}
+        if (!(GPIO->IN & (1 << 14))){
+			uart_send(B);
+		}
+
+        char letter;
+        letter = uart_read();
+
+        if (letter == A) {
+            printf("A");        
+        }
+        else if (letter == B) {
+            printf("B");
+        }
+        
+		sleep = 1000000;
+        while(--sleep); // Delay
     } return 0;
 };
